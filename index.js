3e48a54ea7dde7fdaea9dfd97000024c593f178d
vjcmacrury@gmail.com MacRury <vjcmacrury@gmail.com>
2018-05-19 22:12:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function add2(n) {
   
 }
 
-var theFunk = "FUNKY!"
+
 var funkyFunction = function() {
   return function() {
     return "FUNKY!"
@@ -31,3 +31,4 @@ var funkyFunction = function() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 
+var theFunk = "FUNKY!"
